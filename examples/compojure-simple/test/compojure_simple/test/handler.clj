df976d7537cbdca9f062940bd5d79dbf26d9a0c7
Piotr Jagielski <pjg@touk.pl>
2014-05-06 23:29:59 +0200

    clojure + leiningen example

---
+++ examples/compojure-simple/test/compojure_simple/test/handler.clj
@@ -0,0 +1,14 @@
+(ns compojure-simple.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        compojure-simple.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
